86fdad5b05107e44fe03df2ed261342bb7e37a30
Tunde James <59791126+tunde-james@users.noreply.github.com>
2025-07-08 18:14:53 +0100

    Create the user sign in function

--- apps/api/src/auth/auth.resolver.ts
+++ apps/api/src/auth/auth.resolver.ts
@@ -1,7 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+
 import { AuthService } from './auth.service';
+import { SignInInput } from './dto/signin.input';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => String)
+  async signIn(@Args('signInInput') signInInput: SignInInput) {
+    const user = await this.authService.validateLocalUser(signInInput);
+
+    return 'Login Successful!';
+  }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as argon2 from 'argon2';
+
+import { SignInInput } from './dto/signin.input';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async validateLocalUser({ email, password }: SignInInput) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('User Not Found');
+
+    const passwordMatched = await argon2.verify(user.password, password);
+
+    if (!passwordMatched) {
+      throw new UnauthorizedException('Invalid Credentials!');
+    }
+
+    return user;
+  }
+}
